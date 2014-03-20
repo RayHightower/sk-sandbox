c48bcaabb27b242106a7117ce5f5eb0be6966a26
Raymond T. Hightower <rth@wisdomgroup.com>
2014-03-20 15:28:12 -0500

    creating the first scene

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,9 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    first_scene = UIViewController.alloc.init
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.rootViewController = first_scene
+    @window.makeKeyAndVisible
     true
   end
 end
